1e43800027cdf9b06c5584f8a6bfc9d0bbc4cd4d
ducky <luckyduck0734@gmail.com>
2022-01-15 15:53:13 -0600

    salmon run updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pretty = {
   "League Battle": [0xf12f77, "<:LeagueBattle:931329264450670623>"],
 };
 
-const mapUpdate = async () => {
+const battleUpdate = async () => {
   const request = await axios({
     method: "GET",
     url: "https://splatoon2.ink/data/schedules.json",
@@ -20,7 +20,10 @@ const mapUpdate = async () => {
   for (const mode in data) {
     let battle = data[mode];
     let prettyEmbed = pretty[battle[0].game_mode.name];
-    let nextBattle = battle[1].game_mode.name !== "Regular Battle" ? `(${battle[1].rule.name})`: ""
+    let nextBattle =
+      battle[1].game_mode.name !== "Regular Battle"
+        ? `(${battle[1].rule.name})`
+        : "";
     let embed = {
       title: `${prettyEmbed[1]} ${battle[0].rule.name} (${battle[0].game_mode.name})`,
       description: `
@@ -43,10 +46,56 @@ const mapUpdate = async () => {
       embeds,
     },
   });
-  const now = new Date()
-  const endTime = data.regular[0].end_time
-  const diff = endTime - (Math.round(now.getTime() / 1000)) + 180
-  setTimeout(mapUpdate, diff * 1000);
+  const now = new Date();
+  const endTime = data.regular[0].end_time;
+  const diff = endTime - Math.round(now.getTime() / 1000) + 180;
+  setTimeout(battleUpdate, diff * 1000);
 };
 
-mapUpdate();
+battleUpdate();
+
+const salmon_runUpdate = async () => {
+  const req = await axios({
+    method: "GET",
+    url: "https://splatoon2.ink/data/coop-schedules.json",
+    headers: {
+      "User-Agent": "Thanks! ducky#8930",
+    },
+  });
+  const { details } = req.data;
+  let weapons = [];
+  for (const weaponID of details[0].weapons) {
+    let weapon = weaponID.weapon;
+    weapons.length !== 3
+      ? weapons.push(`${weapon.name}, `)
+      : weapons.push(weapon.name);
+  }
+  const embed = {
+    title: "Salmon Run",
+    color: 0xf24c15,
+    description: `
+    > **Map:** ${details[0].stage.name}
+    > **Next:** ${details[1].stage.name}
+    > **Weapons:** ${weapons.join("")}
+    > **Started:** <t:${details[0].start_time}:R>
+    > **Ends:** <t:${details[0].end_time}:R>
+    `,
+    image: {
+      url: `https://app.splatoon2.nintendo.net${details[0].stage.image}`,
+      name: details[0].stage.name,
+    },
+  };
+  axios({
+    method: "POST",
+    url: process.env.webhook,
+    data: {
+      embeds: [embed],
+    },
+  });
+  const now = new Date();
+  const endTime = details[0].end_time;
+  const diff = endTime - Math.round(now.getTime() / 1000) + 180;
+  setTimeout(battleUpdate, diff * 1000);
+};
+
+salmon_runUpdate();
